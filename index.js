3d036785aa7a8de0e053f1d0095ddae83fe003d1
Jackson Fung <jacksonf21@hotmail.com>
2019-08-01 21:20:26 +0000

    ISS Spotter IV complete

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const { fetchMyIP , fetchCoordsByIP } = require('./iss');
+const { nextISSTimesForMyLocation} = require('./iss');
 
-fetchMyIP((error, ip) => {
+//INITS OVERALL ASYNCS
+nextISSTimesForMyLocation((error, val) => {
+  
   if (error) {
-    console.log('It didn\'t work!', error);
-    return;
-  }
-
-  console.log('It worked! Returned IP:', ip);
-
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log('It didn\'t work!', error);
-      return;
-    }
-
-    console.log('It worked!', coords);
-  }); 
+    return console.log('It didn\'t work!', error);
+  };
+  
+  getDate(val);
 });
 
+//CONVERTS FINAL VAL ABOVE TO READABLE TEXT
+const getDate = (passTimes) => {
+  for (let pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+//FETCHES IP (1ST ASYNC)
 const fetchMyIP = (callback) => {
   request('https://api.ipify.org?format=json',(err, response, body) => {
     
@@ -21,6 +22,7 @@ const fetchMyIP = (callback) => {
   });
 };
 
+//FETCHES COORDS WITH 1ST ASYNC'S DATA (2ND ASYNC)
 const fetchCoordsByIP = (ip, callback) => {
   const geoLocation = 'https://ipvigilante.com/' + ip;
   request(geoLocation, (err, response, body) => {
@@ -41,5 +43,50 @@ const fetchCoordsByIP = (ip, callback) => {
   });
 };
 
+//FETCHES ISSPASSTIMES WITH 2ND ASYNC'S DATA (3RD ASYNC)
+const fetchISSPassTimes = (coords, callback) => {
+  let { latitude, longitude } = coords;
+  request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
 
-module.exports = { fetchMyIP , fetchCoordsByIP };
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS Pass Times. Response: ${body}`;
+      callback(msg, null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+
+  });
+};
+
+//CALLS ALL ASYNCS IN ORDER VIA NESTING AND RETURNS THE RESULT ONLY IF ALL ARE SUCCESSFUL, ELSE RETURN ERROR
+const nextISSTimesForMyLocation = (callback) => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSPassTimes(coords, (error, passTimes) => {
+        if (error) {
+          return callback(error, null)
+        }
+
+        callback(null, passTimes);
+      });
+    });
+  });
+};
+
+
+
+module.exports = { nextISSTimesForMyLocation };
